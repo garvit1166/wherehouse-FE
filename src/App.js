a5c6d158aaae460123abf491d9559a3c8b8409fb
garvit <garvitchutani07@gmail.com>
2024-02-24 03:44:08 +0530

    fix: fix indentation issue

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import MainPage from './pages/MainPage/MainPage';
- 
+
 function App() {
-  return (
-    <div className="App">
-    <Routes>
-      <Route path="/" element={<MainPage/>}>
-          <Route index element={<Home />} />
-      </Route>
-    </Routes>
-  </div>
-  );
+ return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<MainPage/>}>
+          <Route index element={<Home />}/>
+        </Route>
+      </Routes>
+    </div>
+);
 }
  
-export default App;
+export default App;
